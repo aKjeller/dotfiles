38053e43683b8a0cddceadfc4b6f869ff1515674
aKjeller <44802724+aKjeller@users.noreply.github.com>
2025-11-23 13:30:20 +0100

    feat: show parent dir in lualine

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1051,7 +1051,7 @@ require("lazy").setup({
 				sections = {
 					lualine_a = { "mode" },
 					lualine_b = { "branch", "diff", "diagnostics" },
-					lualine_c = { "filename" },
+					lualine_c = { { "filename", path = 4 } },
 					lualine_x = {
 						{
 							"copilot",
