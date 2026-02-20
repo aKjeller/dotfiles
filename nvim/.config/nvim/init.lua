bd8c39cfc6e21efd2b43a1bbf3ead192c556127b
aKjeller <44802724+aKjeller@users.noreply.github.com>
2026-02-20 12:29:35 +0100

    feat(nvim): add 99 plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -480,6 +480,43 @@ require("lazy").setup({
 		end,
 	},
 
+	-- 99
+	{
+		"ThePrimeagen/99",
+		config = function()
+			local _99 = require("99")
+
+			local cwd = vim.uv.cwd()
+			local basename = vim.fs.basename(cwd)
+			_99.setup({
+				provider = _99.OpenCodeProvider,
+				model = "openai/gpt-5.3-codex",
+
+				logger = {
+					level = _99.DEBUG,
+					path = "/tmp/" .. basename .. ".99.debug",
+					print_on_error = true,
+				},
+
+				completion = {
+					custom_rules = {},
+					files = {},
+					source = "blink",
+				},
+
+				md_files = {},
+			})
+
+			vim.keymap.set("v", "<leader>9v", function()
+				_99.visual()
+			end)
+
+			vim.keymap.set("v", "<leader>9s", function()
+				_99.stop_all_requests()
+			end)
+		end,
+	},
+
 	-- LSP Plugins
 	{
 		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
